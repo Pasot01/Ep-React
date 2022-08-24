35fa35f267b5f61bcd8e506678f18f6f368756ef
Pasot01 <contactmyphone14@gmail.com>
2022-08-24 21:33:28 +0200

    Configuration de base d'une app React

---
+++ src/data.js
@@ -0,0 +1,17 @@
+module.exports = [
+    {
+        _id: 1346794613,
+        title: "post 1",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla elit lorem, suscipit nec ipsum nec, lobortis tempor dui. Donec non ex eu risus posuere sodales vitae sed turpis. Ut nec blandit nisl. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla pharetra, lectus vitae ullamcorper rutrum, lacus turpis imperdiet nisl, sit amet luctus tortor nisl vitae quam. Integer lacus nunc, porta vel quam eu, condimentum ullamcorper justo."
+    },
+    {
+        _id: 2356891245,
+        title: "post 2",
+        content: "In finibus aliquet mauris, vel elementum metus cursus vel. Curabitur nec nibh at tellus posuere sagittis. Maecenas imperdiet imperdiet luctus. Pellentesque ut porttitor mi. Morbi et mauris sit amet massa elementum sodales. Integer varius risus vulputate cursus interdum. Nunc ornare commodo vulputate. Donec volutpat fermentum venenatis. Aenean imperdiet justo egestas, tristique felis non, facilisis eros. Quisque vitae lorem in sapien sollicitudin volutpat."
+    },
+    {
+        _id: 1256785610,
+        title: "post 3",
+        content: "Suspendisse vitae finibus tellus, non hendrerit nulla. Morbi ipsum purus, iaculis et semper sodales, aliquet sed magna. Proin ut venenatis orci, in fermentum nulla. Nunc vel neque vitae felis tincidunt finibus nec in lacus. Morbi luctus suscipit varius. Phasellus sed ipsum quis lacus ultricies elementum vel sed justo. Donec tempus nibh sit amet rutrum consequat. Nullam eget lobortis sem. Integer bibendum enim vel ligula sagittis, a pharetra ante bibendum."
+    }
+]
